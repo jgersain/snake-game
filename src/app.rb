cfa2f718d976e75ac548cb3382bc9a1a3885eaee
jgersain <jhersein@gmail.com>
2019-07-23 13:17:09 -0500

    rendered snake and your food

--- src/app.rb
+++ src/app.rb
@@ -1,1 +1,8 @@
-puts "hello world"
+require_relative "model/state.rb"
+require_relative "view/ruby2d"
+
+view = View::Ruby2dView.new
+
+initial_state = Model::initial_state
+
+view.render(initial_state)
