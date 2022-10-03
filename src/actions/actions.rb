11efb9268992813ff126d0197d1c4ce4d12a52b7
jgersain <jhersein@gmail.com>
2022-10-02 19:38:36 -0500

    Prepare the snake's change direction

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -3,7 +3,7 @@ require_relative '../model/state'
 
 module Actions
   def self.move_snake(state)
-    next_direction = state.next_direction
+    next_direction = state.curr_direction
     next_position = calc_next_position(state)
     if position_is_valid?(state, next_position)
       move_snake_to(state, next_position)
@@ -12,11 +12,20 @@ module Actions
     end
   end
 
+  def self.change_direction(state, direction)
+    if next_direction_is_valid?(state, direction)
+      state.curr_direction = direction
+    else
+      puts 'Invalid direction!'
+    end
+    state
+  end
+
   private
 
   def self.calc_next_position(state)
     curr_position = state.snake.positions.first
-    case state.next_direction
+    case state.curr_direction
     when Model::Direction::UP
       return Model::Coord.new(
         curr_position.row - 1,
@@ -56,5 +65,19 @@ module Actions
   def self.end_game(state)
     state.game_finished = true
     state
+  end
+
+  def self.next_direction_is_valid?(state, direction)
+    case state.curr_direction
+    when Model::Direction::UP
+      return true if direction != Model::Direction::DOWN
+    when Model::Direction::DOWN
+      return true if direction != Model::Direction::UP
+    when Model::Direction::RIGHT
+      return true if direction != Model::Direction::LEFT
+    when Model::Direction::LEFT
+      return true if direction != Model::Direction::RIGHT
+    end
+    return false
   end
 end
--- tests/actions_tests.rb
+++ tests/actions_tests.rb
@@ -5,8 +5,8 @@ require_relative '../src/actions/actions'
 require_relative '../src/model/state'
 
 class ActionsTest < Minitest::Test
-  def test_move_snake
-    initial_state = Model::State.new(
+  def setup
+    @initial_state = Model::State.new(
       Model::Snake.new([
           Model::Coord.new(1, 1),
           Model::Coord.new(0, 1),
@@ -16,7 +16,9 @@ class ActionsTest < Minitest::Test
       Model::Direction::DOWN,
       false
     )
+  end
 
+  def test_move_snake
     expected_state = Model::State.new(
       Model::Snake.new([
           Model::Coord.new(2, 1),
@@ -28,7 +30,38 @@ class ActionsTest < Minitest::Test
       false
     )
 
-    actual_state = Actions::move_snake(initial_state)
+    actual_state = Actions::move_snake(@initial_state)
     assert_equal actual_state, expected_state
+  end
+
+  def test_change_direction_invalid
+    expected_state = Model::State.new(
+      Model::Snake.new([
+        Model::Coord.new(1, 1),
+        Model::Coord.new(0, 1),
+      ]),
+      Model::Food.new(4, 4),
+      Model::Grid.new(8, 12),
+      Model::Direction::DOWN,
+      false
+    )
+    actual_state = Actions::change_direction(@initial_state, Model::Direction::UP)
+    assert_equal actual_state, expected_state
+  end
+
+  def test_change_directon_valid
+    expected_state = Model::State.new(
+      Model::Snake.new([
+        Model::Coord.new(1, 1),
+        Model::Coord.new(0, 1),
+      ]),
+      Model::Food.new(4, 4),
+      Model::Grid.new(8, 12),
+      Model::Direction::LEFT,
+      false
+    )
+    actual_state = Actions::change_direction(@initial_state, Model::Direction::LEFT)
+    assert_equal actual_state, expected_state
+
   end
 end
