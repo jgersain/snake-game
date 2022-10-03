1bc9deac4ef03abe6addfc04699c8df417e8583b
jgersain <jhersein@gmail.com>
2022-10-02 23:33:48 -0500

    Grow snake when it eats food

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -5,7 +5,9 @@ module Actions
   def self.move_snake(state)
     next_direction = state.curr_direction
     next_position = calc_next_position(state)
-    if position_is_valid?(state, next_position)
+    if position_is_food?(state, next_position)
+      grow_snake_to(state, next_position)
+    elsif position_is_valid?(state, next_position)
       move_snake_to(state, next_position)
     else
       end_game(state)
@@ -22,6 +24,15 @@ module Actions
   end
 
   private
+
+  def self.position_is_food?(state, position)
+    state.food.row == position.row && state.food.col == position.col
+  end
+
+  def self.grow_snake_to(state, position)
+    state.snake.positions = [position] + state.snake.positions
+    state
+  end
 
   def self.calc_next_position(state)
     curr_position = state.snake.positions.first
